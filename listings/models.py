f16120934ce08f8f0b1a17f42b0bda4287cfd41b
Hell4U <18BCE131@nirmauni.ac.in>
2021-10-16 11:58:47 +0530

    Made the listing model and configure the static file setting

---
+++ listings/models.py
@@ -0,0 +1,34 @@
+import uuid
+from datetime import datetime
+
+import pytz
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+class Listing(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    address2 = models.CharField(max_length=200, blank=True)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    zipcode = models.CharField(max_length=200)
+    description = models.TextField(max_length=200)
+    price = models.IntegerField()
+    bedrooms = models.IntegerField()
+    bathrooms = models.IntegerField()
+    sqft = models.IntegerField()
+    type = models.CharField(max_length=20)
+    feature_image = models.ImageField(upload_to="house/")
+    image_1 = models.ImageField(upload_to="house/")
+    image_2 = models.ImageField(upload_to="house/")
+    image_3 = models.ImageField(upload_to="house/")
+    image_4 = models.ImageField(upload_to="house/")
+    is_published = models.BooleanField(default=True)
+    list_date = models.DateTimeField(default=datetime.now(pytz.timezone("Asia/Kolkata")))
+    id=models.UUIDField(default= uuid.uuid4, unique=True, primary_key=True, editable=False )
+
+    def __str__(self):
+        return self.title
